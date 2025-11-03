b3370f1e8ff54c6df1590b0e7926ca89702c123a
David Maza <davidmazar@hotmail.com>
2025-11-03 15:01:02 -0500

    Se agrega el campo status al momento de crear/modificar una sede

--- src/pages/Branches/Branches.tsx
+++ src/pages/Branches/Branches.tsx
@@ -362,6 +362,16 @@ const Branches = () => {
                                 </div>
 
                                 <div>
+                                    <label className="block text-sm font-medium text-gray-700">Status</label>
+                                    <select name="status" value={formData.status || ''} onChange={handleSelectChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-primary focus:border-primary">
+                                        <option value="">-- Select Status --</option>
+                                        <option value="A">Active</option>
+                                        <option value="I">Inactive</option>
+                                        
+                                    </select> 
+                                </div>
+
+                                <div>
                                     <label className="block text-sm font-medium text-gray-700">Timezone</label>
                                     <input name="timezone" value={formData.timezone} onChange={handleInputChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-primary focus:border-primary" />
                                 </div>
